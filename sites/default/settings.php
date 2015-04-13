6cd90ff7ee28d4b9aa231e38b4fc88c32e1b063a
unknown <charles.russell@WTCH02112013025.webteksinc.com>
2015-04-13 14:13:26 -0400

    Modified settings.php to enable caching in the environments where they should be enabled, also put in place the frame work for the google analytics to change between environments.

--- sites/default/settings.php
+++ sites/default/settings.php
@@ -211,9 +211,9 @@
  * @endcode
  */
 $databases = array (
-  'default' => 
+  'default' =>
   array (
-    'default' => 
+    'default' =>
     array (
       'database' => 'infotectest',
       'username' => 'infotecusertest',
@@ -565,3 +565,51 @@ $conf['404_fast_html'] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML+RDFa 1.0//EN"
  * Remove the leading hash signs to disable.
  */
 # $conf['allow_authorize_operations'] = FALSE;
+
+// All Pantheon Environments.
+if (defined('PANTHEON_ENVIRONMENT')) {
+  // Drupal caching in development environments.
+  if (!in_array(PANTHEON_ENVIRONMENT, array('test', 'live'))) {
+    // Anonymous caching.
+    $conf['cache'] = 0;
+    // Block caching - disabled.
+    $conf['block_cache'] = 0;
+    // Expiration of cached pages - none.
+    $conf['page_cache_maximum_age'] = 0;
+    // Aggregate and compress CSS files in Drupal - off.
+    $conf['preprocess_css'] = 0;
+    // Aggregate JavaScript files in Drupal - off.
+    $conf['preprocess_js'] = 0;
+  }
+  // Drupal caching in test and live environments.
+  else {
+    // Anonymous caching - enabled.
+    $conf['cache'] = 1;
+    // Block caching - enabled.
+    $conf['block_cache'] = 1;
+    // Expiration of cached pages - 15 minutes.
+    $conf['page_cache_maximum_age'] = 900;
+    // Aggregate and compress CSS files in Drupal - on.
+    $conf['preprocess_css'] = 1;
+    // Aggregate JavaScript files in Drupal - on.
+    $conf['preprocess_js'] = 1;
+  }
+  // Minimum cache lifetime - always none.
+  $conf['cache_lifetime'] = 0;
+  // Cached page compression - always off.
+  $conf['page_compression'] = 0;
+
+
+  if (PANTHEON_ENVIRONMENT == 'dev') {
+    // Google Analytics.
+  // $conf['googleanalytics_account'] = 'UA-XXXXXXXX-X';
+  }
+  else if (PANTHEON_ENVIRONMENT == 'test') {
+    // Google Analytics.
+  // $conf['googleanalytics_account'] = 'UA-XXXXXXXX-Y';
+  }
+  else if (PANTHEON_ENVIRONMENT == 'live') {
+    // Google Analytics.
+  // $conf['googleanalytics_account'] = 'UA-XXXXXXXX-Z';
+  }
+}
